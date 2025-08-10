b976848d48d341d7fb0a1840f22bffa44ed10b0c
Muad-Ahmed <muad8010@gmail.com>
2025-08-10 11:48:34 +0300

    add the main skeleton of navbar

---
+++ components/helper/ShoppingCartButton.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ShoppingCartButton = () => {
+  return (
+    <div>ShoppingCartButton</div>
+  )
+}
+
+export default ShoppingCartButton
--- components/home/Nav.tsx
+++ components/home/Nav.tsx
@@ -1,7 +1,27 @@
+import Link from "next/link";
 import React from "react";
+import Image from "next/image";
+import SearchBox from "../helper/SearchBox";
+import { HeartIcon, UserIcon } from "lucide-react";
+import ShoppingCartButton from "../helper/ShoppingCartButton";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <div className="h-[12vh] sticky top-0 z-[1] bg-white shadow-md ">
+      <div className="flex items-center justify-between w-[95%] md:w-4/5 mx-auto h-full">
+        {/* Logo */}
+        <Link href="/">
+          <Image src="/images/logo.png" alt="logo" width={140} height={140} />
+        </Link>
+        <div className="flex items-center space-x-6">
+          <SearchBox /> {/*Component*/}
+          <HeartIcon size="26" cursor="pointer" />
+          <ShoppingCartButton /> {/*Component*/}
+          <UserIcon size="26" cursor="pointer" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Nav;
