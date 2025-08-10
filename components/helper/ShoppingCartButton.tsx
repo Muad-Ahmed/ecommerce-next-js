440d3c739878fe48a8fbab5187eca3b2cfb5f1b0
Muad-Ahmed <muad8010@gmail.com>
2025-08-10 11:57:24 +0300

    add ShoppingCartBUtton component

--- components/helper/ShoppingCartButton.tsx
+++ components/helper/ShoppingCartButton.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { ShoppingBagIcon } from "lucide-react";
+import React from "react";
 
 const ShoppingCartButton = () => {
   return (
-    <div>ShoppingCartButton</div>
-  )
-}
+    <div className="relative">
+      <span
+        className="absolute -top-3 -right-3 w-6 h-6 bg-red-500 text-center 
+      flex items-center justify-center flex-col text-xs text-white rounded-full"
+      >
+        6
+      </span>
+      <ShoppingBagIcon size={26} cursor={"pointer"} />
+    </div>
+  );
+};
 
-export default ShoppingCartButton
+export default ShoppingCartButton;
