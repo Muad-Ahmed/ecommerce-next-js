c7e1201491303e919f0a70fd540b45c000feb890
Muad-Ahmed <muad8010@gmail.com>
2025-08-11 11:23:04 +0300

    add categories section with responsive

--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import Hero from './Hero'
+import React from "react";
+import Hero from "./Hero";
+import Category from "./Category";
 
 const Home = () => {
   return (
-    <Hero />
-  )
-}
+    <>
+      <Hero />
+      <Category />
+    </>
+  );
+};
 
-export default Home
+export default Home;
