47c5f32b92cc606710c5351aca09450492f25ff8
Muad-Ahmed <muad8010@gmail.com>
2025-08-19 11:03:25 +0300

    add CartSidebar style and logic

--- components/helper/ShoppingCartButton.tsx
+++ components/helper/ShoppingCartButton.tsx
@@ -1,22 +1,33 @@
+"use client";
+
 import { RootState } from "@/store/store";
 import { ShoppingBagIcon } from "lucide-react";
 import React from "react";
 import { useSelector } from "react-redux";
+import { SheetTrigger, Sheet, SheetContent } from "../ui/sheet";
+import CartSidebar from "./CartSidebar";
 
 const ShoppingCartButton = () => {
   const items = useSelector((rootState: RootState) => rootState.cart.items);
   const totalQuantity = items.reduce((total, item) => total + item.quantity, 0);
 
   return (
-    <div className="relative">
-      <span
-        className="absolute -top-3 -right-3 w-6 h-6 bg-red-500 text-center 
+    <Sheet>
+      <SheetTrigger>
+        <div className="relative">
+          <span
+            className="absolute -top-3 -right-3 w-6 h-6 bg-red-500 text-center 
       flex items-center justify-center flex-col text-xs text-white rounded-full"
-      >
-        {totalQuantity}
-      </span>
-      <ShoppingBagIcon size={26} cursor={"pointer"} />
-    </div>
+          >
+            {totalQuantity}
+          </span>
+          <ShoppingBagIcon size={26} cursor={"pointer"} />
+        </div>
+      </SheetTrigger>
+      <SheetContent className="overflow-auto h-full">
+        <CartSidebar items={items} />
+      </SheetContent>
+    </Sheet>
   );
 };
 
