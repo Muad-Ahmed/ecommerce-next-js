3dd19e444010c189f255361048dd8e045470f48a
Muad-Ahmed <muad8010@gmail.com>
2026-01-11 11:37:38 +0300

    Implement error handling in API fetch functions
    Added error handling to fetch functions to return empty arrays on failure.

--- request/requests.ts
+++ request/requests.ts
@@ -2,11 +2,17 @@ export async function getAllCategories() {
   const categoryRes = await fetch(
     "https://fakestoreapi.com/products/categories"
   );
+  if (!categoryRes.ok) {
+        return []; 
+    }
   return categoryRes.json();
 }
 
 export async function getAllProducts() {
   const productsRes = await fetch("https://fakestoreapi.com/products");
+  if (!productsRes.ok) {
+        return []; 
+    }
   return productsRes.json();
 }
 
@@ -14,6 +20,9 @@ export async function getSingleProduct(id: string) {
   const singleProductRes = await fetch(
     `https://fakestoreapi.com/products/${id}`
   );
+  if (!singleProductRes.ok) {
+        return []; 
+    }
   return singleProductRes.json();
 }
 
@@ -21,5 +30,8 @@ export async function getProductByCategory(category: string) {
   const productByCategoryRes = await fetch(
     `https://fakestoreapi.com/products/category/${category}`
   );
+  if (!productByCategoryRes.ok) {
+        return []; 
+    }
   return productByCategoryRes.json();
 }
