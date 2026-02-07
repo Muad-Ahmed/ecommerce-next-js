34acd9db84dac0a6bdc3e867d20d8268fe5b20ec
Muad-Ahmed <muad8010@gmail.com>
2026-02-07 08:51:11 +0300

    fix: disable automatic scroll restoration on refresh

--- StoreProvider/StoreProvider.tsx
+++ StoreProvider/StoreProvider.tsx
@@ -1,10 +1,14 @@
-'use client';
+"use client";
 
 import store from "@/store/store";
-import React from "react";
+import React, { useEffect } from "react";
 import { Provider } from "react-redux";
 
 const StoreProvider = ({ children }: { children: React.ReactNode }) => {
+  // sroll to top when reload
+  useEffect(() => {
+    window.history.scrollRestoration = "manual";
+  }, []);
   return <Provider store={store}>{children}</Provider>;
 };
 
